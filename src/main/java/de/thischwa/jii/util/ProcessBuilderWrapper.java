e20b7f5261cbb18e66380f57fa6118c9a819a919
Thilo Schwarz <thilo.schwarz@googlemail.com>
2014-02-20 07:39:37 +0100

    use available utils to close the BufferedReader

--- src/main/java/de/thischwa/jii/util/ProcessBuilderWrapper.java
+++ src/main/java/de/thischwa/jii/util/ProcessBuilderWrapper.java
@@ -20,7 +20,6 @@ package de.thischwa.jii.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -126,11 +125,7 @@ public class ProcessBuilderWrapper {
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				IOUtil.closeQuietly(br);
 			}
 		}
 	}	
